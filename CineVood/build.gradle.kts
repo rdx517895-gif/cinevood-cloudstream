54c5bc3b076550fa1a58aec6899445b9592cf31a
rdx517895-gif <rdx517895@gmail.com>
2026-03-29 18:19:26 +0530

    Add initial build configuration for CineVood

---
+++ CineVood/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "CineVood – Bollywood Hollywood Tamil Telugu Malayalam"
+    authors      = listOf("rdx517895-gif")
+    language     = "en"
+    status       = 1
+    tvTypes      = listOf("Movie", "TvSeries")
+    iconUrl      = "https://one.1cinevood.watch/wp-content/uploads/2020/07/cvlogo.png"
+}
