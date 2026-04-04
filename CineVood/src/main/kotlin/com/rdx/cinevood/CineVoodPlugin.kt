34ce29494e2945b31435aa915541ad164d67157f
rdx517895-gif <rdx517895@gmail.com>
2026-04-04 10:28:00 +0530

    Modify load function to accept Context parameter

--- CineVood/src/main/kotlin/com/rdx/cinevood/CineVoodPlugin.kt
+++ CineVood/src/main/kotlin/com/rdx/cinevood/CineVoodPlugin.kt
@@ -1,11 +1,12 @@
 package com.rdx.cinevood
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class CineVoodPlugin : BasePlugin() {
-    override fun load() {
+    override fun load(context: Context) {
         registerMainAPI(CineVoodProvider())
     }
 }
