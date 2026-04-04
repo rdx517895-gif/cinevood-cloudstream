2b0adb555735a69dabc8bb97e9a0c71c19c6b16d
rdx517895-gif <rdx517895@gmail.com>
2026-04-04 09:54:45 +0530

    Change language from 'en' to 'hi' in build.gradle.kts

--- CineVood/build.gradle.kts
+++ CineVood/build.gradle.kts
@@ -3,8 +3,12 @@ version = 1
 cloudstream {
     description = "CineVood – Bollywood Hollywood Tamil Telugu Malayalam"
     authors      = listOf("rdx517895-gif")
-    language     = "en"
+    language     = "hi"
     status       = 1
     tvTypes      = listOf("Movie", "TvSeries")
     iconUrl      = "https://one.1cinevood.watch/wp-content/uploads/2020/07/cvlogo.png"
+}
+
+dependencies {
+    implementation("com.lagradost:cloudstream3:pre-release")
 }
