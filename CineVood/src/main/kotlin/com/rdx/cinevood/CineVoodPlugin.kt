301c6d35bd9460da950ec3928006368f7a6d7ceb
rdx517895-gif <rdx517895@gmail.com>
2026-04-04 10:44:19 +0530

    Remove Context parameter from load method

--- CineVood/src/main/kotlin/com/rdx/cinevood/CineVoodPlugin.kt
+++ CineVood/src/main/kotlin/com/rdx/cinevood/CineVoodPlugin.kt
@@ -1,12 +1,11 @@
 package com.rdx.cinevood
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class CineVoodPlugin : BasePlugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerMainAPI(CineVoodProvider())
     }
 }
